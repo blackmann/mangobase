e195033518a4110adc09bb8321a612c37677a32c
Degreat <mail@degreat.co.uk>
2023-05-23 20:47:43 +0000

    Manifest, add/get collection

---
+++ base/src/manifest.test.ts
@@ -0,0 +1,36 @@
+import { afterAll, describe, expect, it } from 'vitest'
+import { rm, stat } from 'fs/promises'
+import Manifest from './manifest'
+import assert from 'assert'
+
+describe('Manifest', () => {
+  const manifest = new Manifest()
+
+  afterAll(async () => {
+    try {
+      // this is a safeguard so that files are not mistakenly removed
+      assert(Manifest.getDirectory().startsWith('.mb_'))
+      await rm(Manifest.getDirectory(), { recursive: true })
+    } catch (err) {
+      console.log(err)
+    }
+  })
+
+  describe('collections', () => {
+    it('adds and saves collection config', async () => {
+      await manifest.collection('mock', {
+        name: 'mock',
+        schema: { name: { type: 'string' } },
+      })
+
+      expect(
+        stat([Manifest.getDirectory(), 'collections.json'].join('/'))
+      ).toBeDefined()
+
+      expect(await manifest.collection('mock')).toStrictEqual({
+        name: 'mock',
+        schema: { name: { type: 'string' } },
+      })
+    })
+  })
+})
--- base/src/manifest.ts
+++ base/src/manifest.ts
@@ -1,14 +1,55 @@
-class Manifest {
-  private initialize: Promise<void>
+import { mkdir, stat, writeFile } from 'fs/promises'
+import { SchemaDefinitions } from './schema'
 
-  constructor() {
-    this.initialize = (async () => {
-      // load existing manifest file(s) and populate
-    })()
+interface CollectionConfig {
+  name: string
+  schema: SchemaDefinitions
+  /**
+   * If `true`, a service will be created for this collection.
+   * Default is `true`
+   */
+  exposed?: boolean
+  /**
+   * If `true`, this collection will be the schema used to validate
+   * fields of other schemas.
+   * Default is `false`
+   */
+  template?: boolean
+}
+
+class Manifest {
+  collections: Record<string, CollectionConfig> = {}
+
+  async collection(
+    name: string,
+    config?: CollectionConfig
+  ): Promise<CollectionConfig> {
+    if (!config) {
+      return this.collections[name]
+    }
+
+    this.collections[name] = config
+    await this.save()
+
+    return config
   }
 
-  async getSchema(collection: string): Promise<any> {
-    await this.initialize
+  async save(env?: string) {
+    const dir = Manifest.getDirectory(env)
+
+    try {
+      await stat(dir)
+    } catch (err) {
+      await mkdir(dir)
+    }
+    const collectionsJson = JSON.stringify(this.collections, undefined, 2)
+    await writeFile([dir, 'collections.json'].join('/'), collectionsJson, {
+      encoding: 'utf-8',
+    })
+  }
+
+  static getDirectory(env?: string) {
+    return `.mb_${env || process.env.NODE_ENV}`
   }
 }
 
