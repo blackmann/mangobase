c126a2c4ce0905b823ae6a6600fd48fbdc187aa0
Degreat <mail@degreat.co.uk>
2024-01-18 08:10:07 +0000

    feat: snack to show feedback messages

--- admin/src/app.tsx
+++ admin/src/app.tsx
@@ -1,6 +1,12 @@
 import { RouterProvider } from 'react-router-dom'
+import { Snacks } from './components/snacks'
 import routes from './routes'
 
 export function App() {
-  return <RouterProvider router={routes} />
+  return (
+    <>
+      <RouterProvider router={routes} />
+      <Snacks />
+    </>
+  )
 }
